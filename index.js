b084f0fca6a6cb56fe1883d47b2b30cbc26f35fd
Kaila <kailaamia@gmail.com>
2023-11-10 17:06:54 -0600

    Removed pseudo code

--- index.js
+++ index.js
@@ -2,19 +2,6 @@ const inquirer = require('inquirer') //Inquirer module
 const fs = require('fs') //File system module
 
 //When node is called?
-    //...Show questions
-        //Use inquirer.prompt([])
-            //the answers are stored in an object
-        //'What is the title of your project?'
-        //'What is a description of your project ?'
-        //'What are you instructions for installation?'
-        //'What are the instructions for usage?'
-        //'What are your guidelines for contributing ?'
-        //'What are the instructions to run tests?'
-        //'Which license are you using?'
-            //'MIT License','Apache License 2.0','GNU General Public License (GPL)', 'None'
-        //'What is your github username?'
-        //'What is your email address?'
     //When show questions is called?
         //Get user input from answers object
         //add answers to readme template
