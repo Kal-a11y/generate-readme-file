2b599efc84af35c878aac877a5153531936acaf5
Kaila <kailaamia@gmail.com>
2023-11-13 16:46:08 -0600

    Added code to init()

--- index.js
+++ index.js
@@ -70,7 +70,13 @@ function writeToFile(fileName, data) {
 }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+    .then(data => {
+        const content = generateMarkdown.generateMarkdown(data)
+        writeToFile('./testReadme.md',content)
+    })
+}
 
 // Function call to initialize app
 init();
