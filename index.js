e256c700309caed54c096535749f8b848052772f
Kaila <kailaamia@gmail.com>
2023-11-13 15:16:07 -0600

    Created readme template literal

--- index.js
+++ index.js
@@ -82,3 +82,51 @@ const getLicenseBadge = license => {
     }
     return badge
 }
+
+const readme = `
+    # (Title here)
+
+    (badge image here)
+
+    ## Description
+
+    (description text here)
+
+    ## Table of Contents 
+
+    [Description](#description) \
+    [Table of Contents](#table-of-contents) \
+    [Installlation](#installlation) \
+    [Usage](#usage) \
+    [License](#license) \
+    [Contributing](#contributing) \
+    [Tests](#tests) \
+    [Questions](#questions) 
+
+
+    ## Installation
+
+    (installation text here)
+
+    ## Usage
+
+    (usage text here)
+
+    ## License
+
+    (license text here)
+
+    ## Contributing
+
+    (contribution text here)
+
+    ## Tests
+
+    (test text here)
+
+    ## Questions
+
+    GitHub: [username here](github link here)
+    Email: email here
+
+`
