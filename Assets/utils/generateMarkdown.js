9c3b529a1fc921bacd534aac2e4d125f80a1b6d1
Kaila <kailaamia@gmail.com>
2023-11-13 20:23:22 -0600

    Fixed blank license case

--- Assets/utils/generateMarkdown.js
+++ Assets/utils/generateMarkdown.js
@@ -12,7 +12,7 @@ function renderLicenseBadge(license) {
         case 'GNU General Public License (GPL)':
             badge = '[![License: GPL v3](https://img.shields.io/badge/License-GPL%20v3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)'
             break;
-        case '':
+        case 'None':
           badge = ''
           break;
     }
@@ -33,7 +33,7 @@ function renderLicenseLink(license) {
         case 'GNU General Public License (GPL)':
             link = 'https://www.gnu.org/licenses/gpl-3.0.en.html'
             break;
-        case '':
+        case 'None':
           link = ''
           break;
     }
